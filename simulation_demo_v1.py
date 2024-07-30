07c40f2168f8f03951feb1f831f9ca749649be86
brjo0501 <61231045+brjo0501@users.noreply.github.com>
2024-07-30 16:44:27 +0200

    RCA - Simulation Gripper Intervention complete and running, addition of simple data collecting based on intervention timestamp, graph refresh after HT root cause analysis

--- simulation_demo_v1.py
+++ simulation_demo_v1.py
@@ -1,3 +1,4 @@
+from kivy.core.window import Window
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
@@ -9,7 +10,7 @@ from kivy.uix.button import Button
 from kivy_garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 from kivy.graphics import Color, Rectangle
 from kivy.clock import Clock
-from kivy.core.window import Window
+
 import math
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -91,10 +92,10 @@ class MainLayout(WhiteBoxLayout):
         self.normal_data = pd.DataFrame()
         self.abnormal_data = pd.DataFrame()
 
-        client = RemoteAPIClient()
+        self.client = RemoteAPIClient()
 
-        self.sim = client.require('sim')
-        self.simBWF = client.require('simBWF')
+        self.sim = self.client.require('sim')
+        self.simBWF = self.client.require('simBWF')
 
         self.inter_script = self.sim.getObject('/Interventions')
 
@@ -115,6 +116,7 @@ class MainLayout(WhiteBoxLayout):
         self.events = self.sim.getObject('/Events')
 
         self.inter = {'Normal':'normal','Gripper 1':'interGripper1','Gripper 2':'interGripper2'}
+
 
         # Left Panel for 6 Graphs
         self.left_panel = WhiteGridLayout(cols=1, size_hint_x=0.5)
@@ -151,10 +153,10 @@ class MainLayout(WhiteBoxLayout):
         box_layout.add_widget(center_layout)
         center_layout = AnchorLayout(anchor_x='center', anchor_y='center')
 
-        self.button3 = Button(text='Pause Simulation', size_hint=(None, None), size=(400, 60), background_color=(0.9, 0.9, 0.9, 1))
-        center_layout.add_widget(self.button3)
-        box_layout.add_widget(center_layout)
-        center_layout = AnchorLayout(anchor_x='center', anchor_y='center')
+        # self.button3 = Button(text='Pause Simulation', size_hint=(None, None), size=(400, 60), background_color=(0.9, 0.9, 0.9, 1))
+        # center_layout.add_widget(self.button3)
+        # box_layout.add_widget(center_layout)
+        # center_layout = AnchorLayout(anchor_x='center', anchor_y='center')
 
         # Select Box (Spinner)
         self.spinner1 = Spinner(
@@ -201,12 +203,12 @@ class MainLayout(WhiteBoxLayout):
         box_layout2.add_widget(self.networkx_graph_display)
         right_panel.add_widget(box_layout2)
 
-        self.networkx_graph_display.figure = self.create_networkx_graph()
+        self.networkx_graph_display.figure = self.create_networkx_graph(self.colors)
         self.networkx_graph_display.draw()
 
         self.button1.bind(on_press=self.start_sim)
         self.button2.bind(on_press=self.stop_sim)
-        self.button3.bind(on_press=self.pause_sim)
+        #self.button3.bind(on_press=self.pause_sim)
         
         self.spinner1.bind(text=self.on_spinner_select_1)
         self.spinner2.bind(text=self.on_spinner_select_2)
@@ -214,9 +216,9 @@ class MainLayout(WhiteBoxLayout):
         self.add_widget(self.left_panel)
         self.add_widget(right_panel)
 
-    def create_networkx_graph(self):
+    def create_networkx_graph(self,node_colors):
         G = nx.DiGraph()
-        fig, ax = plt.subplots(figsize=(12, 10))
+        fig, ax = plt.subplots(figsize=(12, 8))
         ax.set_facecolor('#f0f8ff')  # Very light blue background
         # Draw the graph
         nodes = [
@@ -324,7 +326,7 @@ class MainLayout(WhiteBoxLayout):
         ax.set_xlim(-12,14)
         ax.set_ylim(-12,8)
         ax.set_title(f'Causal Graph:', fontsize=12)
-        nx.draw(G, pos,with_labels=True,node_size=1500, node_color=[self.colors[node] for node in G.nodes()], font_size=6, arrowsize=8,width=0.5)
+        nx.draw(G, pos,with_labels=True,node_size=1500, node_color=[node_colors[node] for node in G.nodes()], font_size=6, arrowsize=8,width=0.5)
         return fig
 
     def on_button_press(self, instance):
@@ -332,33 +334,40 @@ class MainLayout(WhiteBoxLayout):
         if self.networkx_graph_display.figure.gca().has_data():
             self.networkx_graph_display.figure = plt.figure()
         else:
-            self.networkx_graph_display.figure = self.create_networkx_graph()
+            self.networkx_graph_display.figure = self.create_networkx_graph(self.colors)
         self.networkx_graph_display.draw()
     
     def stop_sim(self, instance):
         self.sim.stopSimulation()
+        print('Stop Simulation')
         self.clock_data.cancel()
         self.clock_plot.cancel()
-        # Clear the figures when stopping the simulation
+
+        fig_score = self.canvas_score.figure
+        ax_score = fig_score.axes[0]
+        ax_score.cla()
+        self.canvas_score.draw()
+
         for canvas in self.graphs:
             ax = canvas.figure.gca()  # Get the current axes
             ax.cla()  # Clear the current axes
             canvas.draw()
-        
+
         self.t_data = pd.DataFrame()
         self.data_out = pd.DataFrame()
-        self.graphs = []
-        self.scores = []
         
         #self.sim.setBoolParam(self.sim.boolparam_display_enabled, True)
 
-    def pause_sim(self, instance):
-        self.sim.pauseSimulation()
-        self.clock_data.cancel()
-        self.clock_plot.cancel()
+    # def pause_sim(self, instance):
+    #     self.sim.setBoolParam(self.sim.boolparam_display_enabled, True)
+    #     self.sim.pauseSimulation()
+    #     print('Pause Simulation')
+    #     self.clock_data.cancel()
+    #     self.clock_plot.cancel()
                 
     def start_sim(self, instance):
-        #self.sim.setStepping(True)
+
+        self.sim.setStepping(True)
         self.sim.startSimulation()
         self.sim.setBoolParam(self.sim.boolparam_display_enabled, False)
         print('Connection and Start Simulation')
@@ -366,14 +375,33 @@ class MainLayout(WhiteBoxLayout):
         self.update_interval_data = 0.050  # Update interval in seconds
         self.clock_data = Clock.schedule_interval(self.update_data, self.update_interval_data)
 
-        # Schedule update every second
         self.update_interval = 3  # Update interval in seconds
-        self.time_elapsed = 0.0  # Time elapsed in seconds
         self.clock_plot = Clock.schedule_interval(self.update_plots, self.update_interval)
-        # Schedule update every second
+        self.sim.step()
+
+    def detect_anomalies(self,data):
+        window_size = 10
+        threshold = 20
+        earliest_anomaly_index = None
+        data = data[['cam_1_X', 'cam_1_Y', 'cam_2_X', 'cam_2_Y', 'cam_3_X', 'cam_3_Y',
+                    'rob_1_maxVel', 'rob_1_supply',
+                    'rob_2_maxVel', 'rob_2_supply']]
+        for column in data.columns:
+            moving_avg = data[column].rolling(window=window_size).mean()
+            diff = (data[column] - moving_avg).abs()
+            anomaly_indices = diff[diff > threshold*moving_avg/100].index # 20% difference
+            if not anomaly_indices.empty:
+                first_anomaly_index = anomaly_indices[0]
+                if earliest_anomaly_index is None or first_anomaly_index < earliest_anomaly_index:
+                    earliest_anomaly_index = first_anomaly_index
+        print(self.t_data['time'][earliest_anomaly_index])
+        return earliest_anomaly_index
+
         
     def update_data(self,dt):
+        
         all_data_row = pd.DataFrame()
+
         for obj in self.objects:
             obj_data = pd.DataFrame([self.sim.unpackTable(self.sim.readCustomDataBlock(obj,'customData'))])
             obj_data = self.data_process(obj_data)
@@ -395,37 +423,63 @@ class MainLayout(WhiteBoxLayout):
             score = (non_zero_count / total_count) * 100
             self.scores.append(score)
             score_dict = pd.DataFrame({'score':[score]})
-            all_data_row = pd.concat([all_data_row,score_dict],axis=1)
+            all_data_row = pd.concat([all_data_row,self.data_process(score_dict)],axis=1)
+
+        #print(all_data_row)
 
         self.data_out = pd.concat([self.data_out, all_data_row],ignore_index=True)
         #print(self.data_out)
         self.t_data = pd.concat([self.t_data,pd.DataFrame({'time': [self.sim.getSimulationTime()]})], ignore_index=True)
         #print(self.sim.getSimulationTime())
-        #self.sim.step()
+        self.sim.step()
 
     def run_RCA(self):
 
+        self.clock_data.cancel()
+        self.clock_plot.cancel()
+
         last_time = self.t_data['time'].iloc[-1]
-        time_threshold = last_time - 35
 
-        abnormal_index= self.t_data[self.t_data['time'] <= time_threshold-1]
+        abnormal_index = self.t_data[self.t_data['time'] <= last_time-self.anomaly_gap].index[-1]
 
-        warm_up_index = self.t_data[self.t_data['time'] <= 3-1]
+
+        warm_up_index =self.t_data[self.t_data['time'] <= 35].index[-1]
+
+        print(self.detect_anomalies(self.data_out.loc[warm_up_index:]))
+
+
+        #print(warm_up_index,abnormal_index) # at least 1 min before introduction of intervention
+
+        results = []
 
         if warm_up_index < abnormal_index:
-            self.abnormal_data = self.data_out.loc[abnormal_index:]
-            self.normal_data = self.data_out.loc[warm_up_index:abnormal_index]
+            # self.abnormal_data = self.data_out.loc[abnormal_index:]
+            # self.normal_data = self.data_out.loc[warm_up_index:abnormal_index]
 
+            self.abnormal_data = self.data_out.loc[self.intervention_index:]
+            self.normal_data = self.data_out.loc[warm_up_index:self.intervention_index]
+
+            main_dir = 'G:\My Drive\Master Thesis\Simulation\Test'
+            folder_name = f'Test_1'
+            folder_path = os.path.join(main_dir, folder_name)
+            os.makedirs(folder_path, exist_ok=True)
+            filename = 'abnormal.csv'
+            self.abnormal_data.to_csv(os.path.join(folder_path, filename), index=False)
+
+            filename = 'normal.csv'
+            self.normal_data.to_csv(os.path.join(folder_path, filename), index=False)
+
+            self.sim.setStepping(False)
             self.sim.pauseSimulation()
-            self.clock_data.cancel()
-            self.clock_plot.cancel()
 
-            self.run_HT(abnormal_df = self.abnormal_data,
+            results = self.run_HT(abnormal_df = self.abnormal_data,
                         normal_df = self.normal_data,
                         key_nodes = ['score'],
                         colors = self.colors)
             
         self.rca_activated = False
+
+        print(results)
 
     def on_spinner_select_1(self, spinner, text):
         
@@ -470,6 +524,8 @@ class MainLayout(WhiteBoxLayout):
         elif self.graph_count < num_graphs:
             for i in range(num_graphs-self.graph_count):
                 self.remove_graph()
+        
+        self.update_plots(3)
 
     def add_graph(self):
         # Create a new figure and add it to the layout
@@ -487,6 +543,8 @@ class MainLayout(WhiteBoxLayout):
 
     def on_spinner_select_2(self, spinner, text):
         if text == 'Gripper 1' or text == 'Gripper 2':
+            self.anomaly_gap = 22.65
+            self.intervention_index = self.t_data['time'].index[-1]
             self.sim.callScriptFunction(self.inter['Normal'],
                                         self.sim.getScript(self.sim.scripttype_customizationscript, self.inter_script))
             self.sim.callScriptFunction(self.inter[text],
@@ -494,47 +552,56 @@ class MainLayout(WhiteBoxLayout):
         elif text == 'Normal':
             self.sim.callScriptFunction(self.inter[text],
                                         self.sim.getScript(self.sim.scripttype_customizationscript, self.inter_script))
+            if self.sim.getSimulationState() != self.sim.simulation_stopped:
+                self.normal_index = self.t_data['time'].index[-1]
     
-    def select_rename_columns(self,obj,data):
+    def select_rename_columns(self,obj,data:pd.DataFrame):
         if obj == self.camera_1:
-            data = data[['sizeX','sizeY']]
-            data = data.rename(columns={'sizeX': 'cam_1_X','sizeY': 'cam_1_Y'})
+            target_columns = ['sizeX', 'sizeY']
+            target_rename = {'sizeX': 'cam_1_X', 'sizeY': 'cam_1_Y'}
         elif obj == self.camera_2:
-            data = data[['sizeX','sizeY']]
-            data = data.rename(columns={'sizeX': 'cam_2_X','sizeY': 'cam_2_Y'})
+            target_columns = ['sizeX', 'sizeY']
+            target_rename = {'sizeX': 'cam_2_X', 'sizeY': 'cam_2_Y'}
         elif obj == self.camera_3:
-            data = data[['sizeX','sizeY']]
-            data = data.rename(columns={'sizeX': 'cam_3_X','sizeY': 'cam_3_Y'})
+            target_columns = ['sizeX', 'sizeY']
+            target_rename = {'sizeX': 'cam_3_X', 'sizeY': 'cam_3_Y'}
         elif obj == self.conveyor_1:
-            data = data[['speed']]
-            data = data.rename(columns={'speed':'con_1'})
+            target_columns = ['speed']
+            target_rename = {'speed': 'con_1'}
         elif obj == self.conveyor_2:
-            data = data[['speed']]
-            data = data.rename(columns={'speed':'con_2'})
+            target_columns = ['speed']
+            target_rename = {'speed': 'con_2'}
         elif obj == self.conveyor_3:
-            data = data[['speed']]
-            data = data.rename(columns={'speed':'con_3'})
+            target_columns = ['speed']
+            target_rename = {'speed': 'con_3'}
         elif obj == self.camera_EoL:
-            target_columns = ['part1SizeX','part2SizeX','part3SizeX','part4SizeX',
-                              'part1SizeY','part2SizeY','part3SizeY','part4SizeY',
-                              'tray1SizeX','tray1SizeY','tray2SizeX','tray2SizeY']
-            target_rename = {'part1SizeX':'EoL_3_X','part2SizeX':'EoL_4_X','part3SizeX':'EoL_5_X','part4SizeX':'EoL_6_X',
-                                        'part1SizeY':'EoL_3_Y','part2SizeY':'EoL_4_Y','part3SizeY':'EoL_5_Y', 'part4SizeY':'EoL_6_Y',
-                                        'tray1SizeX':'EoL_1_X','tray1SizeY':'EoL_1_Y',
-                                        'tray2SizeX':'EoL_2_X','tray2SizeY':'EoL_2_Y'}
-            present_columns = [col for col in target_columns if col in data.columns]
-            data = data[present_columns]
-            data = data.rename(columns=target_rename)
+            target_columns = ['part1SizeX', 'part2SizeX', 'part3SizeX', 'part4SizeX',
+                            'part1SizeY', 'part2SizeY', 'part3SizeY', 'part4SizeY',
+                            'tray1SizeX', 'tray1SizeY', 'tray2SizeX', 'tray2SizeY']
+            target_rename = {'part1SizeX': 'EoL_3_X', 'part2SizeX': 'EoL_4_X', 'part3SizeX': 'EoL_5_X', 'part4SizeX': 'EoL_6_X',
+                            'part1SizeY': 'EoL_3_Y', 'part2SizeY': 'EoL_4_Y', 'part3SizeY': 'EoL_5_Y', 'part4SizeY': 'EoL_6_Y',
+                            'tray1SizeX': 'EoL_1_X', 'tray1SizeY': 'EoL_1_Y',
+                            'tray2SizeX': 'EoL_2_X', 'tray2SizeY': 'EoL_2_Y'}
         elif obj == self.rob_1:
-            data = data[['jointVelo1','jointVelo2', 'jointVelo4',	'maxVel','gripperSupply','gripperVacuum','jointVelo3']]
-            data = data.rename(columns={'jointVelo1':'rob_1_1','jointVelo2':'rob_1_2','jointVelo3':'rob_1_3','jointVelo4':'rob_1_4',
-                                        'maxVel':'rob_1_maxVel','gripperSupply':'rob_1_supply','gripperVacuum':'rob_1_vacuum'})
+            target_columns = ['jointVelo1', 'jointVelo2', 'jointVelo4', 'maxVel', 'gripperSupply', 'gripperVacuum', 'jointVelo3']
+            target_rename = {'jointVelo1': 'rob_1_1', 'jointVelo2': 'rob_1_2', 'jointVelo3': 'rob_1_3', 'jointVelo4': 'rob_1_4',
+                            'maxVel': 'rob_1_maxVel', 'gripperSupply': 'rob_1_supply', 'gripperVacuum': 'rob_1_vacuum'}
         elif obj == self.rob_2:
-            data = data[['jointVelo1','jointVelo2', 'jointVelo4',	'maxVel','gripperSupply','gripperVacuum','jointVelo3']]
-            data = data.rename(columns={'jointVelo1':'rob_2_1','jointVelo2':'rob_2_2','jointVelo3':'rob_2_3','jointVelo4':'rob_2_4',
-                                        'maxVel':'rob_2_maxVel','gripperSupply':'rob_2_supply','gripperVacuum':'rob_2_vacuum'})
+            target_columns = ['jointVelo1', 'jointVelo2', 'jointVelo4', 'maxVel', 'gripperSupply', 'gripperVacuum', 'jointVelo3']
+            target_rename = {'jointVelo1': 'rob_2_1', 'jointVelo2': 'rob_2_2', 'jointVelo3': 'rob_2_3', 'jointVelo4': 'rob_2_4',
+                            'maxVel': 'rob_2_maxVel', 'gripperSupply': 'rob_2_supply', 'gripperVacuum': 'rob_2_vacuum'}
+
+        # Ensure all target columns are in the data, filling missing ones with 0
+        for col in target_columns:
+            if col not in data.columns:
+                data[col] = 0
+
+        data = data[target_columns]
+        data = data.rename(columns=target_rename)
+        data = data.fillna(0)  # Ensure no NaN values are left
+
         return data
-    
+        
     def select_columns(self,data):
         if self.object == self.camera_1:
             data = data[['cam_1_X','cam_1_Y']]
@@ -566,15 +633,15 @@ class MainLayout(WhiteBoxLayout):
                          'EoL_3_Y','EoL_4_Y','EoL_5_Y', 'EoL_6_Y',
                          'EoL_1_X','EoL_1_Y',
                          'EoL_2_X','EoL_2_Y']]
+        data = data.fillna(0)
         return data
 
     def data_process(self,data : pd.DataFrame):
         return data.replace(np.nan, 0).replace({True: 1, False: 0})
 
-    def update_plots(self, dt):
+    def update_plots(self,dt):
         
-        if self.sim.getSimulationTime() > 0:
-            self.time_elapsed += self.update_interval
+        if self.sim.getSimulationTime() > 0 and self.sim.getSimulationState() != self.sim.simulation_stopped:
             i = 0
             data = self.select_columns(self.data_out)
 
@@ -589,10 +656,10 @@ class MainLayout(WhiteBoxLayout):
                 ax_score.set_xlim(t_start,t_end)
                 ax_score.set_ylim(0,100)
 
-            if self.sim.getSimulationTime() > 35 and not self.anomaly_detected:
+            if self.sim.getSimulationTime() > 33.75 and not self.anomaly_detected:
                 if self.scores[-1] < 100:
                     t_end = self.t_data['time'].iloc[-1]
-                    anomaly.append((t_end-35, t_end))
+                    anomaly.append((self.t_data['time'].iloc[self.intervention_index], t_end)) #self.anomaly_gap
                     self.anomaly_detected = True
                     self.rca_activated = True
                     self.label.text = f'Anomaly Detected:{self.anomaly_detected}'
@@ -634,7 +701,10 @@ class MainLayout(WhiteBoxLayout):
                 # Redraw canvas to reflect updated plot
                 canvas.draw()
                 i +=1
-            
+
+            if self.rca_activated:
+                self.run_RCA()
+                        
 
     def run_HT(self,
             normal_df: pd.DataFrame,
@@ -784,8 +854,11 @@ class MainLayout(WhiteBoxLayout):
 
         for node in rank3_root_cause:
             new_colors[node] = 'lightcoral'
-        print(abnormal_nodes)
-        return [abnormal_nodes,new_colors]  
+
+        self.networkx_graph_display.figure = self.create_networkx_graph(new_colors)
+        self.networkx_graph_display.draw()
+
+        return [[rank1_root_cause,rank2_root_cause,rank3_root_cause]]  
 
 class MyApp(App):
     def build(self):
